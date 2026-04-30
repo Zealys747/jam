294683734eba0fb9b99015d40a017786580eabc6
[a]ster <personal.asterious@gmail.com>
2026-04-30 17:14:43 +0300

    base audio managers

---
+++ Assets/Scripts/Music/MusicLibrary.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct MusicTrack
+{
+    public string trackName;
+    public AudioClip clip;
+}
+
+public class MusicLibrary : MonoBehaviour
+{
+    // !Every music clip must be set "Streaming" in the Load Type!
+    public MusicTrack[] tracks;
+
+    public AudioClip GetClipFromName(string trackName)
+    {
+        foreach (var track in tracks)
+        {
+            if (track.trackName == trackName)
+            {
+                return track.clip;
+            }
+        }
+        
+        return null;
+    }
+}
