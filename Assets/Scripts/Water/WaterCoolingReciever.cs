1c61fb3e698b6dff0c6d5ec5c93d0055adbb40ea
[a]ster <personal.asterious@gmail.com>
2026-05-03 14:42:22 +0300

    timer for water instead of particles

--- Assets/Scripts/Water/WaterCoolingReciever.cs
+++ Assets/Scripts/Water/WaterCoolingReciever.cs
@@ -4,28 +4,24 @@ public class WaterCoolingReciever : MonoBehaviour
 {
     public TemperatureController temperatureController;
 
-    public float coolingPerParticle = 0.1f;
-    public float maxCooling = 2f;
-    private float _coolingThisFrame = 0f;
+    [Range(0f, 1f)] public float waterLevel = 0f; // текущий уровень воды, сюдаа
+    public float fillRate = 0.2f; // скорость наполнения в сек
+    
+    public float coolingWhilePouring = 1f;
+    private bool _isPouring = false;
 
     private void Update()
     {
-        if (temperatureController != null)
-        {
-            temperatureController.waterCooling = _coolingThisFrame;
-        }
-
-        if (_coolingThisFrame > 0)
-        {
-            Debug.Log("ОООООО ЕБАТЬ ЛЬЕТСЯ ВОДИЦА");
-        }
-        _coolingThisFrame = 0f;
         
+        if (_isPouring)
+            waterLevel = Mathf.Clamp01(waterLevel + fillRate * Time.deltaTime);
+
+        
+        if (temperatureController != null)
+            temperatureController.waterCooling = _isPouring ? coolingWhilePouring : 0f;
     }
 
-    private void OnParticleCollision(GameObject other)
-    {
-        _coolingThisFrame = Mathf.Min(_coolingThisFrame + coolingPerParticle, maxCooling);
-    }
+    public void StartPouring() =>  _isPouring = true;
+    public void StopPouring() =>  _isPouring = false;
     
 }
